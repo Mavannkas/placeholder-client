faa5060fb99aa07b7be6f1ed67327bda65622f2a
Wiktor <wiktor.ludwiniak@gmail.com>
2021-09-08 11:21:50 +0200

    Implement main request handling

--- placeholder-api/src/App.js
+++ placeholder-api/src/App.js
@@ -1,5 +1,29 @@
+import { useEffect } from "react";
+import { useHttp } from "./hooks/use-http";
+import { getAllTodos } from "./lib/api";
+
 function App() {
-  return <></>;
+  const { isLoading, result, error, sendRequest } = useHttp(getAllTodos, true);
+
+  useEffect(() => {
+    sendRequest();
+  }, [sendRequest]);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Sth is wrong...</p>;
+  }
+
+  return (
+    <>
+      {result.map((item) => (
+        <div>{item.title}</div>
+      ))}
+    </>
+  );
 }
 
 export default App;
